615f2f4fdf207fd9a9a1ed2ebd42315f18d9383d
SvjetlanaC <75023202+SvjetlanaC@users.noreply.github.com>
2023-05-03 16:32:10 +0200

    Optimizations

---
+++ AR_Projekat2_BezOptimizacija/Ofast optimizacija/main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma pack (1)
+#include <string.h>
+#include <time.h>
+
+typedef struct {
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+}COLOR;
+
+typedef struct
+{
+	unsigned short file_type;
+	unsigned int file_size;
+	unsigned short reserved1;
+	unsigned short reserved2;
+	unsigned int offset;
+}BMP_FILE_HEADER;
+
+typedef struct
+{
+	unsigned int size;
+	long width;
+	long height;
+	unsigned short planes;
+	unsigned short bit_per_px;
+	unsigned int compression;
+	unsigned int img_size;
+	long x_px_m;
+	long y_px_m;
+	unsigned int clr_used;
+	unsigned int clr_important;
+}BMP_INFO;
+
+void sort(BMP_INFO info,unsigned char* buffer){
+
+    int p = 0;
+
+    COLOR t;
+    if(info.width % 4 != 0)
+    {
+        p = 4 - (info.width % 4);
+    }
+    long i,k,l;
+    for(i=0; i < info.height; i++)
+    {
+        for(k=0; k<info.width * 3; k+=3)
+        {
+            int flag=0;
+            //RGB values in bmp are stored backwards i.e. BGR
+            t.B=buffer[i*info.width*3 + i*p + k];
+            t.G=buffer[i*info.width*3 + i*p + k + 1];
+            t.R=buffer[i*info.width*3 + i*p + k + 2];
+            //luminance
+            long s=0.3*t.R + 0.59*t.G + 0.11*t.B;
+
+            for(l=k; l>0 && s < (0.3*buffer[i*info.width*3 + i*p + l-1]+0.59*buffer[i*info.width*3 + i*p + l-2]+0.11*buffer[i*info.width*3 + i*p +l-3]);l-=3)
+            {
+                buffer[i*info.width*3 + i*p + l]=buffer[i*info.width*3 + i*p + l-3];
+                buffer[i*info.width*3 + i*p + l+1]=buffer[i*info.width*3 + i*p + l-2];
+                buffer[i*info.width*3 + i*p + l+2]=buffer[i*info.width*3 + i*p + l-1];
+            }
+
+            buffer[i*info.width*3 + i*p + l]=t.B;
+            buffer[i*info.width*3 + i*p + l+1]=t.G;
+            buffer[i*info.width*3 + i*p + l+2]=t.R;
+        }
+
+    }
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *in, *out;
+	clock_t start,end,total;
+
+	if (argc < 3)
+		printf("Nedovoljan broj argumenata!");
+	else
+	{
+		if ((in = fopen(argv[1], "rb")) != NULL)
+		{
+			BMP_FILE_HEADER h;
+			BMP_INFO info;
+			unsigned char* buffer;
+
+			int  size;
+			fread(&h, sizeof(BMP_FILE_HEADER), 1, in);
+			fread(&info, sizeof(BMP_INFO), 1, in);
+			if (h.file_type != 0x4D42)
+				printf("Nepoznat tip fajla!");
+			else if (info.compression != 0)
+				printf("Fajl je kompresovan!");
+			else if (info.bit_per_px != 24)
+				printf("Fajl nije 24-bitni bmp!");
+			else
+			{
+				size = h.file_size - h.offset; //odredjivanje velicine bafera
+				buffer = (unsigned char*)calloc(size, 1);
+
+				fread(buffer,1, size, in); //citanje vrijednosti piksela
+
+
+                start=clock();
+                sort(info,buffer);
+                end=clock();
+
+                total = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+                printf("Vrijeme izvrsavanja bez optimizacija: %ds%dms \n",total/1000,total%1000);
+
+
+				if ((out = fopen(argv[2], "wb")) != NULL)
+				{
+					fwrite(&h, sizeof(BMP_FILE_HEADER), 1, out);
+					fwrite(&info, sizeof(BMP_INFO), 1, out);
+					fwrite(buffer, 1, size, out);
+					fclose(out);
+				}
+				else
+					printf("Greska prilikom otvaranja izlazne datoteke!");
+				free(buffer);
+			}
+			fclose(in);
+		}
+		else
+			printf("Greska prilikom otvaranja ulazne datoteke!");
+
+	}
+	FILE *info;
+    if((info = fopen("info.txt", "a")) != NULL)
+    {
+        fprintf(info,"Vrijeme izvrsavanja programa sortiranja piksela bez optimizacije: %ds%dms \n",total/1000,total%1000);
+    }
+    else
+        printf("Neuspjesno otvaranje datoteke sa rezultatima mjerenja!\n");
+    fclose(info);
+	return 0;
+}
---
+++ ArhitekturaRacunaraKombinovaneOptimizacije/O3 optimizacija/main.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma pack (1)
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+
+typedef struct {
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+}COLOR;
+
+typedef struct
+{
+	unsigned short file_type;
+	unsigned int file_size;
+	unsigned short reserved1;
+	unsigned short reserved2;
+	unsigned int offset;
+}BMP_FILE_HEADER;
+
+typedef struct
+{
+	unsigned int size;
+	long width;
+	long height;
+	unsigned short planes;
+	unsigned short bit_per_px;
+	unsigned int compression;
+	unsigned int img_size;
+	long x_px_m;
+	long y_px_m;
+	unsigned int clr_used;
+	unsigned int clr_important;
+}BMP_INFO;
+
+int getPadding(BMP_INFO info){
+    int p = 0;
+    if(info.width % 4 != 0)
+        p = 4 - (info.width % 4);
+    return p;
+}
+
+void sortColumns(BMP_INFO info,unsigned char *buffer,int i2,int block,int k1)
+{
+    COLOR t;
+
+    int p = getPadding(info);
+    for(int k2=k1; (k2<k1+block) && (k2<info.width *3); k2+=3)
+    {
+        t.B=buffer[i2*info.width*3 + i2*p + k2];
+        t.G=buffer[i2*info.width*3 + i2*p + k2 + 1];
+        t.R=buffer[i2*info.width*3 + i2*p + k2 + 2];
+        long s=0.3*t.R + 0.59*t.G + 0.11*t.B;
+
+        int l2;
+        for(l2=k2; (l2>0 && s < (0.3*buffer[i2*info.width*3 + i2*p + l2-1]+0.59*buffer[i2*info.width*3 + i2*p + l2-2]+0.11*buffer[i2*info.width*3 + i2*p +l2-3])) ;l2-=3)
+        {
+            buffer[i2*info.width*3 + i2*p + l2]=buffer[i2*info.width*3 + i2*p + l2-3];
+            buffer[i2*info.width*3 + i2*p + l2+1]=buffer[i2*info.width*3 + i2*p + l2-2];
+            buffer[i2*info.width*3 + i2*p + l2+2]=buffer[i2*info.width*3 + i2*p + l2-1];
+        }
+
+        buffer[i2*info.width*3 + i2*p + l2]=t.B;
+        buffer[i2*info.width*3 + i2*p + l2+1]=t.G;
+        buffer[i2*info.width*3 + i2*p + l2+2]=t.R;
+    }
+
+}
+
+void sort(BMP_INFO info, unsigned char *buffer,int block)
+{
+    int pivot=info.height/2;
+    #pragma omp parallel sections
+    {
+
+        #pragma omp section
+        {
+
+            for(int i1=0; i1 < pivot-1; i1+=block){
+                 for(int k1=0; k1<info.width * 3; k1+=block){
+
+                        for(int i2=i1; (i2<i1+block) && (i2<pivot-1); i2++)
+                        {
+                            sortColumns(info,buffer,i2,block,k1);
+                        }
+                    }
+            }
+        }
+
+        #pragma omp section
+        {
+            for(int i1=pivot-1; i1 < info.height; i1+=block){
+                 for(int k1=0; k1<info.width * 3; k1+=block){
+
+                        for(int i2=i1; (i2<i1+block) && (i2<info.height); i2++)
+                        {
+                            sortColumns(info,buffer,i2,block,k1);
+                        }
+
+                 }
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *in, *out;
+	clock_t start,end,total;
+
+	if (argc < 3)
+		printf("Nedovoljan broj argumenata!");
+	else
+	{
+		if ((in = fopen(argv[1], "rb")) != NULL)
+		{
+			BMP_FILE_HEADER h;
+			BMP_INFO info;
+			unsigned char* buffer;
+
+			int  size;
+			fread(&h, sizeof(BMP_FILE_HEADER), 1, in);
+			fread(&info, sizeof(BMP_INFO), 1, in);
+			if (h.file_type != 0x4D42)
+				printf("Nepoznat tip fajla!");
+			else if (info.compression != 0)
+				printf("Fajl je kompresovan!");
+			else if (info.bit_per_px != 24)
+				printf("Fajl nije 24-bitni bmp!");
+			else
+			{
+				size = h.file_size - h.offset;
+				buffer = (unsigned char*)calloc(size, 1);
+				fread(buffer,1, size, in);
+
+                start=clock();
+                sort(info,buffer,3);
+                end=clock();
+
+                total = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+                printf("Vrijeme izvrsavanja sa kombinovanim optimizacijama: %ds%dms",total/1000,total%1000);
+
+
+				if ((out = fopen(argv[2], "wb")) != NULL)
+				{
+					fwrite(&h, sizeof(BMP_FILE_HEADER), 1, out);
+					fwrite(&info, sizeof(BMP_INFO), 1, out);
+					fwrite(buffer, 1, size, out);
+					fclose(out);
+				}
+				else
+					printf("Greska prilikom otvaranja izlazne datoteke!");
+				free(buffer);
+			}
+			fclose(in);
+		}
+		else
+			printf("Greska prilikom otvaranja ulazne datoteke!");
+	}
+	FILE *info;
+    if((info = fopen("info.txt", "a")) != NULL)
+    {
+        fprintf(info,"Vrijeme izvrsavanja sa kombinovanim optimizacijama: %ds%dms \n",total/1000,total%1000);
+    }
+    else
+        printf("Neuspjesno otvaranje datoteke sa rezultatima mjerenja!\n");
+    fclose(info);
+	return 0;
+}
